b13100476fd62c8f2192a1c194370a9125aaf2ab
Nakkikone <nakkikone@Nakkikones-MacBook-Pro.local>
2020-01-21 16:39:56 +0200

    ready 3

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 'use strict';
 const coursesEn = ["Hamburger, cream sauce and poiled potates",
-                "Goan style fish curry and whole grain rice",
-                "Vegan Chili sin carne and whole grain rice",
-                "Broccoli puree soup, side salad with two napas",
-                "Lunch baguette with BBQ-turkey filling",
-                "Cheese / Chicken / Vege / Halloum burger and french fries"];
+    "Goan style fish curry and whole grain rice",
+    "Vegan Chili sin carne and whole grain rice",
+    "Broccoli puree soup, side salad with two napas",
+    "Lunch baguette with BBQ-turkey filling",
+    "Cheese / Chicken / Vege / Halloum burger and french fries"];
 const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-                "Goalaista kalacurrya ja täysjyväriisiä",
-                "vegaani Chili sin carne ja täysjyväriisi",
-                "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-                "Lunch baguette with BBQ-turkey filling",
-                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+    "Goalaista kalacurrya ja täysjyväriisiä",
+    "vegaani Chili sin carne ja täysjyväriisi",
+    "Parsakeittoa,lisäkesalaatti kahdella napaksella",
+    "Lunch baguette with BBQ-turkey filling",
+    "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
 
 const SortEn = coursesEn;
-const menuFi = document.querySelector('.menu-fi');
-const menuEn = document.querySelector('.menu-en');
+const menu = document.querySelector('.menu');
 const langButton = document.querySelector('#lang');
 const sortButton = document.querySelector('#sort');
 let finnish = true;
@@ -23,38 +22,41 @@ coursesFi.sort();
 coursesEn.sort();
 
 coursesFi.forEach(annos => {
-    menuFi.innerHTML += '<li class="course">' + annos +'</li>';
+    menu.innerHTML += '<li class="course">' + annos + '</li>';
 });
 
-coursesEn.forEach(course => {
-    menuEn.innerHTML += '<li class="course">' + course +'</li>';
-});
 
 const changeLang = () => {
-    if(finnish) {
-        menuEn.style.display = 'block';
-        menuFi.style.display = 'none';
+    if (finnish) {
+        menu.innerHTML = '';
+        coursesEn.forEach(course => {
+            menu.innerHTML += '<li class="course">' + course + '</li>';
+        });
         finnish = false;
     } else {
-        menuEn.style.display = 'none';
-        menuFi.style.display = 'block';
+        menu.innerHTML = '';
+        coursesFi.forEach(annos => {
+            menu.innerHTML += '<li class="course">' + annos + '</li>';
+        });
         finnish = true;
-    }
+    };
 };
 
 const sortOrder = () => {
-    coursesFi.reverse();
-    menuFi.innerHTML = '';
-    coursesFi.forEach(annos => {
-        menuFi.innerHTML += '<li class="course">' + annos +'</li>';
-    });
-
-    coursesEn.reverse();
-    menuEn.innerHTML = '';
-    coursesEn.forEach(course => {
-        menuEn.innerHTML += '<li class="course">' + course +'</li>';
-    });
+    if (finnish) {
+        coursesFi.reverse();
+        menu.innerHTML = '';
+        coursesFi.forEach(annos => {
+            menu.innerHTML += '<li class="course">' + annos + '</li>';
+        });
+    } else {
+        coursesEn.reverse();
+        menu.innerHTML = '';
+        coursesEn.forEach(course => {
+            menu.innerHTML += '<li class="course">' + course + '</li>';
+        });
+    };
 };
 
-langButton.addEventListener('click', changeLang);
-sortButton.addEventListener('click', sortOrder);
+    langButton.addEventListener('click', changeLang);
+    sortButton.addEventListener('click', sortOrder);

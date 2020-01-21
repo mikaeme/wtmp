adfaddd443cb4ffac4655560f29666eb722a1c4c
Nakkikone <nakkikone@Nakkikones-MacBook-Pro.local>
2020-01-21 20:37:43 +0200

    final

--- src/index.js
+++ src/index.js
@@ -5,56 +5,64 @@ import LunchMenu from './assets/sodexo.json';
 const menu = document.querySelector('.menu');
 const langButton = document.querySelector('#lang');
 const sortButton = document.querySelector('#sort');
+const randomButton = document.querySelector('#random');
+const coursesFi = [];
+const coursesEn = [];
 let finnish = true;
 
-LunchMenu.courses.sort();
-
 for (let i in LunchMenu.courses) {
-    menu.innerHTML += '<li class="course">' + LunchMenu.courses[i].title_fi + '</li>';
+    coursesFi.push(LunchMenu.courses[i].title_fi);
+    coursesEn.push(LunchMenu.courses[i].title_en);
 };
+coursesFi.sort();
+coursesEn.sort();
+
+menu.innerHTML = '';
+coursesFi.forEach(annos => {
+    menu.innerHTML += '<li class="course">' + annos + '</li>';
+});
+
 
 const changeLang = () => {
     menu.innerHTML = '';
     if (finnish) {
-        for (let i in LunchMenu.courses) {
-            menu.innerHTML += '<li class="course">' + LunchMenu.courses[i].title_en + '</li>';
-        };
+        coursesEn.forEach(annos => {
+            menu.innerHTML += '<li class="course">' + annos + '</li>';
+        });
         finnish = false;
     } else {
-        for (let i in LunchMenu.courses) {
-            menu.innerHTML += '<li class="course">' + LunchMenu.courses[i].title_fi + '</li>';
-        };
+        coursesFi.forEach(annos => {
+            menu.innerHTML += '<li class="course">' + annos + '</li>';
+        });
         finnish = true;
     };
 };
-//         coursesEn.forEach(course => {
-//             menu.innerHTML += '<li class="course">' + course + '</li>';
-//         });
-//         finnish = false;
-//     } else {
-//         menu.innerHTML = '';
-//         coursesFi.forEach(annos => {
-//             menu.innerHTML += '<li class="course">' + annos + '</li>';
-//         });
-//         finnish = true;
-//     };
-// };
 
-//const sortOrder = () => {
-//     if (finnish) {
-//         coursesFi.reverse();
-//         menu.innerHTML = '';
-//         coursesFi.forEach(annos => {
-//             menu.innerHTML += '<li class="course">' + annos + '</li>';
-//         });
-//     } else {
-//         coursesEn.reverse();
-//         menu.innerHTML = '';
-//         coursesEn.forEach(course => {
-//             menu.innerHTML += '<li class="course">' + course + '</li>';
-//         });
-//     };
-// };
+const sortOrder = () => {
+    if (finnish) {
+        coursesFi.reverse();
+        menu.innerHTML = '';
+        coursesFi.forEach(annos => {
+            menu.innerHTML += '<li class="course">' + annos + '</li>';
+        });
+    } else {
+        coursesEn.reverse();
+        menu.innerHTML = '';
+        coursesEn.forEach(course => {
+            menu.innerHTML += '<li class="course">' + course + '</li>';
+        });
+    };
+};
+
+const randomCourse = () => {
+    const random = Math.floor(Math.random()*coursesFi.length)+1;
+    if(finnish) {
+        alert(coursesFi[random]);
+    } else {
+        alert(coursesEn[random]);
+    }
+};
 
 langButton.addEventListener('click', changeLang);
 sortButton.addEventListener('click', sortOrder);
+randomButton.addEventListener('click', randomCourse);

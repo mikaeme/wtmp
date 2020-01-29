8a67f0f8958a9b2ffc9201d82e1551c08cf50f31
Nakkikone <nakkikone@Nakkikones-MacBook-Pro.local>
2020-01-29 23:55:44 +0200

    final

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import {coursesFi, coursesEn} from './modules/sodexo-data';
 import {ChangeLang, finnish} from './modules/language-module';
 import {SortOrder} from './modules/sort-module';
 import {RandomCourse} from './modules/random-module';
+import {fazerFi, fazerEn} from './modules/fazer-data';
 
+console.log(fazerFi);
+console.log(fazerEn);
 const menu = document.querySelector('.menu');
+const menuFazer = document.querySelector('.menuF');
 const langButton = document.querySelector('#lang');
 const sortButton = document.querySelector('#sort');
 const randomButton = document.querySelector('#random');
@@ -12,6 +16,11 @@ const randomButton = document.querySelector('#random');
 menu.innerHTML = '';
 coursesFi.forEach(annos => {
     menu.innerHTML += '<li class="course">' + annos + '</li>';
+});
+
+menuFazer.innerHTML = '';
+fazerFi.forEach(annos => {
+    menuFazer.innerHTML += '<li class="course">' + annos + '</li>';
 });
 
 langButton.addEventListener('click', ChangeLang);
---
+++ src/modules/fazer-data.js
@@ -0,0 +1,26 @@
+import LunchMenu from '../assets/fazer_fi.json';
+import LunchMenuEn from '../assets/fazer_en.json';
+
+const menus = LunchMenu.LunchMenus[0].SetMenus;
+const menusEn = LunchMenuEn.LunchMenus[0].SetMenus;
+const fazerFi = [];
+const fazerEn = [];
+let course = [];
+let courseEn = [];
+
+for (let i in menus) {
+    for (let j in menus[i].Meals) {    
+            course.push(menus[i].Meals[j].Name);
+            courseEn.push(menusEn[i].Meals[j].Name);        
+    };
+    fazerFi.push(course.toString());
+    fazerEn.push(courseEn.toString());
+    course = [];
+    courseEn = [];
+};
+
+
+fazerFi.sort();
+fazerEn.sort();
+
+export {fazerFi, fazerEn};
